b94f854f69af06b2458388f010e1baa1edef1176
Chad Redmond <credmond@mercyhurst.edu>
2026-03-04 15:36:06 +0000

    march4

--- prep.py
+++ prep.py
@@ -1,17 +1,14 @@
 from sqlmodel import Session, select
 from models import engine,Bio,Stats
 import pandas as pd
-from sqlalchemy import func
 
 with Session(engine) as session:
-    
-    
     statement = (
-        select(Bio.first_name,Bio.last_name,Stats.GP)
-        .join(Stats,(Bio.first_name == Stats.first_name)&(Bio.last_name == Stats.last_name))
-        
+        select(Bio.first_name, Bio.last_name, Bio.position, Stats.PTS)
+        .join(Stats, (Stats.first_name == Bio.first_name) & (Stats.last_name == Bio.last_name))
+        .where(Stats.PTS > 10)
     )
     records = session.exec(statement).all()
-    
+
 records_df = pd.DataFrame(records)
 print(records_df)
